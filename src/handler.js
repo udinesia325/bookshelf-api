b3de274d7f23e2a4fed11f473a2bc855280fcdbb
udinesia325 <udinesia325@gmail.com>
2023-03-07 09:29:47 +0700

    update and delete success

--- src/handler.js
+++ src/handler.js
@@ -24,14 +24,93 @@ const storeBook = (request, h) => {
         updatedAt: now,
         insertedAt: now,
     })
-    return {
+    return h.response({
         status: "success",
         message: "Buku berhasil ditambahkan",
         data: {
             "bookId": id
         }
-    }
+    }).code(201)
 
 }
 
-module.exports = { storeBook }
+const allBook = (request, h) => {
+
+    return {
+        "status": "success",
+        "data": {
+            ...books.map(book => {
+                return { id: book.id, name: book.name, publisher: book.publisher }
+            })
+        }
+    }
+}
+const allBookWithid = (request, h) => {
+    const { bookId = null } = request.params
+    const indexTarget = books.findIndex((book) => book.id == bookId)
+    if (indexTarget == -1) {
+        return h.response({
+            "status": "fail",
+            "message": "Buku tidak ditemukan"
+        }).code(404)
+
+    }
+    return {
+        "status": "success",
+        "data": {
+            ...books[indexTarget]
+        }
+    }
+}
+const updateBook = (request, h) => {
+    const { bookId = null } = request.params
+    const payload = request.payload
+    // jika tidak ada name 
+    if (!payload.name) {
+        return h.response({
+            "status": "fail",
+            "message": "Gagal memperbarui buku. Mohon isi nama buku"
+        }).code(400)
+    }
+    // jika readpage lebih besar
+    if (payload.readPage > payload.pageCount) {
+        return h.response({
+            "status": "fail",
+            "message": "Gagal memperbarui buku. readPage tidak boleh lebih besar dari pageCount"
+        }).code(400)
+    }
+    const indexTarget = books.findIndex((book) => book.id == bookId)
+    if (indexTarget == -1) {
+        return h.response({
+            "status": "fail",
+            "message": "Gagal memperbarui buku. Id tidak ditemukan"
+        }).code(404)
+
+    }
+    books[indexTarget] = {
+        ...books[indexTarget],
+        ...payload
+    }
+    return {
+        "status": "success",
+        "message": "Buku berhasil diperbarui"
+    }
+}
+const deleteBook = (request, h) => {
+    const { bookId = null } = request.params
+    const indexTarget = books.findIndex((book) => book.id == bookId)
+    if (indexTarget == -1) {
+        return h.response({
+            "status": "fail",
+            "message": "Buku gagal dihapus. Id tidak ditemukan"
+        }).code(404)
+
+    }
+    books.splice(indexTarget, 1)
+    return {
+        "status": "success",
+        "message": "Buku berhasil dihapus"
+    }
+}
+
+module.exports = { storeBook, allBook, allBookWithid, updateBook, deleteBook }
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,31 @@
-const { storeBook } = require("./handler");
+const { storeBook, allBook, allBookWithid, updateBook, deleteBook } = require("./handler");
 
 module.exports = [
     {
         method: "POST",
         path: "/books",
-        handler:storeBook
-    }
+        handler: storeBook
+    },
+    {
+        method: "GET",
+        path: "/books/{bookId}",
+        handler: allBookWithid
+    },
+    {
+        method: "GET",
+        path: "/books",
+        handler: allBook
+    },
+    {
+        method: "PUT",
+        path: "/books/{bookId}",
+        handler: updateBook
+    },
+    {
+        method: "DELETE",
+        path: "/books/{bookId}",
+        handler: deleteBook
+    },
+
 ]
 
